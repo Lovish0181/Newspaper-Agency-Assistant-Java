d981c00f615e52aa24dd276894fba5aaf554329a
Lovish Jindal <88631544+Lovish0181@users.noreply.github.com>
2021-08-28 23:13:07 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package BillGenerator;
+package Signup;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,8 +12,8 @@ public class Main extends Application
  public void start(@SuppressWarnings("exports") Stage primaryStage) 
    {
 		try {
-				Parent root=(Parent) FXMLLoader.load(getClass().getResource("Bill_Form.fxml")); 
-				Scene scene = new Scene(root,400,500);
+				Parent root=(Parent) FXMLLoader.load(getClass().getResource("Signup_Form.fxml")); 
+				Scene scene = new Scene(root,700,500);
 				//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 				primaryStage.setScene(scene);
 				primaryStage.show();
